24878dee0e73172e4bdbb76b8becaa4f2082ec8c
BoikoAnton1992 <145497164+BoikoAnton1992@users.noreply.github.com>
2024-01-17 16:18:18 +0200

    No coment

--- js/task-4.js
+++ js/task-4.js
@@ -1,8 +1,22 @@
-const getTotalBalanceByGender = (users, gender) => {
-  return users
+const getTotalBalanceByGender = (users, gender) => 
+   users
     .filter(user => user.gender === gender)
     .reduce((totalBalance, user) => totalBalance + user.balance, 0);
-};
+;
+
+
+
+// const getTotalBalanceByGender = (users, gender) => {
+//   let totalBalance = 0;
+
+//   users.forEach(user => {
+//     if (user.gender === gender) {
+//       totalBalance += user.balance;
+//     }
+//   });
+
+//   return totalBalance;
+// };
 
 
 const allUsers = [
