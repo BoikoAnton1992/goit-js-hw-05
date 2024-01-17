c7fd5720ea7f408a13d0aa81a1fec36a046d3969
BoikoAnton1992 <145497164+BoikoAnton1992@users.noreply.github.com>
2024-01-17 21:51:41 +0200

    add toSorted

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 
-const sortByDescendingFriendCount = (users) => users.sort((a, b) =>
+const sortByDescendingFriendCount = (users) => users.toSorted((a, b) =>
     b.friends.length - a.friends.length
 )
 
